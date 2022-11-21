a1f753054bde81d2b44edd52ceb3a920f6b7960d
Daniel Lee <dlee13195@gmail.com>
2022-11-21 15:33:28 -0500

    my_each final

--- W3D1.rb
+++ W3D1.rb
@@ -1,6 +1,13 @@
 class Array
     def my_each(&prc)
-        self.my_each(prc)
+        i = 0
+        while i < self.length 
+            ele = self[i]
+            prc.call(ele)
+            i += 1
+        end
         
+        self
+
     end
 end
