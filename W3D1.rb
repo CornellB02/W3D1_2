fdb4b34f3c9c087067adaa738bcd2cdc8d6cb00e
Daniel Lee <dlee13195@gmail.com>
2022-11-21 16:33:58 -0500

    continuation

--- W3D1.rb
+++ W3D1.rb
@@ -13,11 +13,43 @@ class Array
     end
 
     def my_select(&prc)
-        self.my_each.select { |num| prc.call(num) }
+        result = []
+        i = 0
+        while i < self.length
+            ele = self[i] 
+            if prc.call(ele) == true
+                result << ele
+            end
+        end
+        result
+        #self.my_each.select { |num| prc.call(num) }
     end
 
-    def 
-    
+    def my_reject(&prc)
+        result = []
+        i = 0
+        while i < self.length
+            ele = self[i]
+            if prc.call(ele) == false
+                result << ele
+            end
+        end
+        result
+        #self.my_each.reject { |num| prc.call(num) }
+    end
+
+    def my_any?(&prc)
+        i = 0
+        while i < self.length
+            ele = self[i]
+            if prc.call(ele)    
+            i += 1
+        end
+
+        #self.any? { |ele| prc.call(ele) }
+    end
+
+
 
 
 end
